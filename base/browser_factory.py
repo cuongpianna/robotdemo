8e2427c8568136fb54c24e382a916408b46f6316
cuong nguyen <cuongpianna1996@gmail.com>
2020-05-23 15:40:33 +0700

    fix seleniu

--- base/browser_factory.py
+++ base/browser_factory.py
@@ -38,6 +38,7 @@ class WebDriverFactory:
 
         chrome_options = Options()
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--kiosk")
         chrome_options.add_argument('disable-infobars')
         chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
