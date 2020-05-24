1bd4784729f36b709c31bad7fe7a94e08d76d317
cuong nguyen <cuongpianna1996@gmail.com>
2020-05-24 16:14:12 +0700

    ok

--- base/browser_factory.py
+++ base/browser_factory.py
@@ -38,7 +38,7 @@ class WebDriverFactory:
         """
 
         chrome_options = Options()
-        # chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--no-sandbox')
         # chrome_options.add_argument('--disable-setuid-sandbox')
         # chrome_options.add_argument('--start-maximized')
         # # chrome_options.add_argument("--headless")
