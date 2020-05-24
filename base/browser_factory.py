f9d3f911f3f19091309b10e42883d3a357fccb65
cuong nguyen <cuongpianna1996@gmail.com>
2020-05-24 16:47:13 +0700

    done

--- base/browser_factory.py
+++ base/browser_factory.py
@@ -44,8 +44,8 @@ class WebDriverFactory:
         # # chrome_options.add_argument("--headless")
         # # chrome_options.binary_location = "C:\\path\\to\\chrome.exe"
         # chrome_options.add_argument('--remote-debugging-port=9222')
-        # chrome_options.add_argument('--disable-dev-shm-usage')
-        # chrome_options.add_argument('--disable-dev-shm-using')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-dev-shm-using')
         chrome_options.add_argument('disable-infobars')
         chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
         # chrome_options.add_argument('--disable-gpu')
