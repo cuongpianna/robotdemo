0a1acfd3bb64eaa913f43684a12e210d2b8f87e3
cuong nguyen <cuongpianna1996@gmail.com>
2020-05-24 14:19:20 +0700

    fix

--- base/browser_factory.py
+++ base/browser_factory.py
@@ -40,7 +40,8 @@ class WebDriverFactory:
         chrome_options = Options()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-setuid-sandbox')
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--start-maximized')
+        chrome_options.add_argument("--headless")
         # chrome_options.binary_location = "C:\\path\\to\\chrome.exe"
         chrome_options.add_argument('--remote-debugging-port=9222')
         chrome_options.add_argument('--disable-dev-shm-usage')
