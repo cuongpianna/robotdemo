925b435925430d7b22d16c0145fb3e29d9b5dd71
cuong nguyen <cuongpianna1996@gmail.com>
2020-05-27 07:26:42 +0700

    fix frontend

--- static/js/application.js
+++ static/js/application.js
@@ -17,20 +17,28 @@ $(document).ready(function(){
             numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
         }
         if(msg.number == 1) {
+            $('.row').removeClass('active');
+            $('.row1').addClass('active');
             numbers_string = 'Chế độ điều khiển bằng tay';
         }else if(msg.number == 2) {
+            $('.row').removeClass('active');
+            $('.row2').addClass('active');
             if(msg.status == 1) {
                 numbers_string = '<div>Chế độ điều khiển bằng tay</div><div>Kết nối trạm điều khiển trung tâm thành công</div>';
             }else {
                 numbers_string = '<div>Chế độ điều khiển bằng tay</div><div>Kết nối trạm điều khiển trung tâm thất bại</div>';
             }
         }else if(msg.number == 3) {
+            $('.row').removeClass('active');
+            $('.row3').addClass('active');
             if(msg.status == 1) {
                 numbers_string = '<div>Chế độ điều khiển tự động</div><div>Kết nối trạm điều khiển trung tâm thành công</div>';
             }else {
                 numbers_string = '<div>Chế độ điều khiển bằng tay</div><div>Kết nối trạm điều khiển trung tâm thất bại</div>';
             }
         }else if(msg.number == 4) {
+            $('.row').removeClass('active');
+            $('.row4').addClass('active');
             if(msg.status == 1) {
                 numbers_string = '<div>Chế độ tự động bấm vạch từ</div><div>Kết nối trạm điều khiển trung tâm thành công</div>';
             }else {
