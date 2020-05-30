704d38955bf448fd40e9d77b05524ece29ee774e
cuong nguyen <cuongpianna1996@gmail.com>
2020-05-30 10:04:13 +0700

    check connection

---
+++ check.py
@@ -0,0 +1,11 @@
+import subprocess
+import requests
+
+def check_connection():
+    bash_command = "/usr/bin/google-chrome-stable http://localhost:5000"
+    if(requests.get('dlakskjdlkasdkl').status_code != 200):
+        output = subprocess.check_output(['bash', '-c', bash_command])
+
+
+if __name__ == '__main__':
+    check_connection()
