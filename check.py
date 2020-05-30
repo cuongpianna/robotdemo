16813d5bdfa0d5a7587ab0d5d7cb0d398ffaf776
cuong nguyen <cuongpianna1996@gmail.com>
2020-05-30 10:20:43 +0700

    ko

--- check.py
+++ check.py
@@ -1,10 +1,17 @@
 import subprocess
 import requests
+import urllib
 
 def check_connection():
     bash_command = "/usr/bin/google-chrome-stable http://localhost:5000"
-    if(requests.get('https://nguyenvanhieuu.com/').status_code != 200):
+    try:
+        a = urllib.request.urlopen("http://www.stackoverfozzzs.com").getcode()
+        if a == 404:
+            output = subprocess.check_output(['bash', '-c', bash_command])
+    except:
         output = subprocess.check_output(['bash', '-c', bash_command])
+    # if(requests.head('http://nguyenvanhieu.com/').status_code != 200):
+    #     output = subprocess.check_output(['bash', '-c', bash_command])
 
 
 if __name__ == '__main__':
