b073229900d90f756877729d0a54ff0458429c51
cuong nguyen <cuongpianna1996@gmail.com>
2020-06-18 19:07:41 +0700

    fix

--- static/js/application.js
+++ static/js/application.js
@@ -8,15 +8,19 @@ $(document).ready(function () {
 
     var numbers_received = [];
 
+    var connectionNetwork = -1;
+
     socket.on('tt', function (msg) {
-       var connection = msg.connection;
-       if(connection == 0) { // khong ket noi mang
-            modal.hide()
-       }else{
-           modal.show();
-             // document.getElementById('iframeContent').contentWindow.location.reload();
-           // $('#iframeContent').attr('src', 'http://178.128.26.135/');
-       }
+        var connection = msg.connection;
+        if (connectionNetwork != connection) {
+            connectionNetwork = connection;
+            if (connection == 0) { // khong ket noi mang
+                modal.hide()
+            } else {
+                modal.show();
+            }
+        }
+
     })
 
     setInterval(function () {
@@ -26,7 +30,7 @@ $(document).ready(function () {
     // receive details from server
     socket.on('newUdp', function (msg) {
         console.log("Received number" + msg.number);
-         socket.emit('status',msg.number);
+        socket.emit('status', msg.number);
         //maintain a list of ten numbers
         if (numbers_received.length >= 10) {
             numbers_received.shift()
@@ -38,7 +42,7 @@ $(document).ready(function () {
             numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
         }
 
-        e = msg.number;
+        e = msg.status;
 
         if (msg.number == 1) {
             modal.hide();
@@ -46,39 +50,44 @@ $(document).ready(function () {
             status = 'LED Base_Station  OFF + LED Manual_Mode ON';
             $('#mode').html(numbers_string);
             $('.robot-info').html(status);
+            $('iframe').attr('src', '');
 
         } else if (msg.number == 2) {
             numbers_string = 'Chế độ điều khiển bằng tay';
             status = "LED Base_Station ON + LED Manual_Mode ON";
             $('#mode').html(numbers_string);
             $('.robot-info').html(status);
-            modal.hide();
+            var src = $('iframe').attr('src');
+            if (src == '')
+                $('iframe').attr('src', 'https://157.230.245.8/f');
+
             if (msg.status == 1) {
                 setTimeout(function () {
                     modal.show();
-                }, 1000)
+                }, 4000)
             }
+
         } else if (msg.number == 3) {
             numbers_string = 'Chế độ điều khiển tự động';
             status = 'LED Base_Station ON + LED Manual_Mode ON';
             $('#mode').html(numbers_string);
             $('.robot-info').html(status);
-            modal.hide();
+            // modal.hide();
             if (msg.status == 1) {
                 setTimeout(function () {
                     modal.show();
-                }, 1000)
+                }, 4000)
             }
         } else if (msg.number == 4) {
             numbers_string = 'Chế độ tự động bấm vạch từ';
             status = 'LED Base_Station ON + LED Auto_Following_Line_Mode ON';
             $('#mode').html(numbers_string);
             $('.robot-info').html(status);
-            modal.hide();
+            // modal.hide();
             if (msg.status == 1) {
                 setTimeout(function () {
                     modal.show();
-                }, 1000)
+                }, 4000)
             }
         }
     });
