a2abc02b3588c329e3c77dfbbab62c8de403804f
cuong nguyen <cuongpianna1996@gmail.com>
2020-09-04 09:29:58 +0700

    Final

--- get_status_UDP.py
+++ get_status_UDP.py
@@ -1,26 +1,34 @@
 import threading
 import socket
-from constant import UDP_IP_STATUS, PORT2
-from websocket import create_connection
+from constant import UDP_IP_STATUS, ROBOT_CODE, LOCAL_WS, SERVER_WS, IS_PRODUCT, PORT2
+from websocket import create_connection, WebSocket
 import base64
-#========================================================================================#
+import ssl
 
-sock_get_status = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock_get_status.bind((UDP_IP_STATUS, PORT2))
+
+# ========================================================================================
+
 
 def GET_STATUS():
-	while True:
-		msg_robot_status, addr = sock_get_status.recvfrom(1024)
-		if (addr[0] == UDP_IP_STATUS):
-			print(msg_robot_status)
-			ws = create_connection("ws://localhost:49411/downloadMedia")
-			# ws = WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
-			# ws.connect('wss://178.128.26.135/agency/downloadMedia')
-			# ws = create_connection("wss://178.128.26.135/agency/downloadMedia")
-			ws.send(base64.b64encode(bytes(msg_robot_status.decode('utf-8') + '#' + '127.0.0.1:7000', "utf-8")))
-			ws.close()
+    if IS_PRODUCT:
+        ws_url = SERVER_WS
+    else:
+        ws_url = LOCAL_WS
+    while True:
+        try:
+            socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+            socket_udp.bind((UDP_IP_STATUS, PORT2))
+            msg_robot_status, addr = socket_udp.recvfrom(1024)
+            if (addr[0] == UDP_IP_STATUS):
+                print(msg_robot_status)
+                ws = WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+                ws.connect()
+                ws.send(base64.b64encode(bytes(msg_robot_status.decode('utf-8') + '#' + ROBOT_CODE, "utf-8")))
+                ws.close()
+            socket_udp.close()
+        except:
+            pass
 
 
 if __name__ == '__main__':
-	threading.Thread(target=GET_STATUS).start()
-
+    threading.Thread(target=GET_STATUS).start()
